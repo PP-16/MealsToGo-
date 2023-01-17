42139af3afc0acf0deb27ef3cea20ccd84ed90f4
PP-16 <ploypantika45@gmail.com>
2023-01-17 14:27:34 +0700

    SVG Image

--- src/features/restaurants/components/restaurant-info-card.component.jsx
+++ src/features/restaurants/components/restaurant-info-card.component.jsx
@@ -2,27 +2,35 @@ import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 import React from "react";
 import styled from "styled-components/native";
+import star from "../../../../assets/star";
+import { SvgXml } from "react-native-svg";
 
 const RestaurantCard = styled(Card)`
-  backgroundcolor: ${(props) => props.theme.colors.bg.secondary};
+  backgroundColor: ${(props) => props.theme.colors.bg.secondary};
 `;
 const RestaurantCardCover = styled(Card.Cover)`
   padding: ${(props) => props.theme.space[3]};
-  backgroundcolor: ${(props) => props.theme.colors.bg.secondary};
+  backgroundColor: ${(props) => props.theme.colors.bg.secondary};
 `;
 const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
+  font-Family: ${(props) => props.theme.fonts.heading};
+  font-Size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 const Address = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
+  font-Family: ${(props) => props.theme.fonts.body};
+  font-Size: ${(props) => props.theme.fontSizes.caption};
 `;
 
 const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-Direction: row;
+  padding-Top: ${(props) => props.theme.space[2]};
+  padding-Bottom: ${(props) => props.theme.space[2]};
 `;
 
 export const RestaurantInfoCard = ({ ...restaurant }) => {
@@ -38,12 +46,19 @@ export const RestaurantInfoCard = ({ ...restaurant }) => {
     isClosedTemporarily,
   } = restaurant;
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
   return (
     <>
       <RestaurantCard elevation={5}>
         <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
         <Info>
           <Title>{name}</Title>
+          <Rating>
+            {ratingArray.map((_, index) => (
+              <SvgXml key={index} xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+
           <Address>{address}</Address>
         </Info>
       </RestaurantCard>
